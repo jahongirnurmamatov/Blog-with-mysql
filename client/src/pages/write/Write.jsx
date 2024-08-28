7980ce5cba02dda67b5338ff3e03c10fe4335560
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-28 20:42:59 +0500

    Write a post page

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -1,9 +1,72 @@
-import './write.scss'
+import { useState } from "react";
+import "./write.scss";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Write = () => {
-  return (
-    <div>Write</div>
-  )
-}
+  const [value, setValue] = useState("");
 
-export default Write
+  return (
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuill
+            className="editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b>Status:</b> Draft
+          </span>
+          <span>
+            <b>Visibility:</b> Public
+          </span>
+          <input type="file" id="file" style={{ display: "none" }} />
+          <label className="file" htmlFor="file">
+            Upload Image
+          </label>
+          <div className="buttons">
+            <button>Save as a draft</button>
+            <button>Update</button>
+          </div>
+        </div>
+        <div className="item">
+          <h1>Category</h1>
+          <div className="cat">
+            <input type="radio" id="art" name="cat" value="art" />
+            <label htmlFor="art">Art</label>
+          </div>
+          <div className="cat">
+            <input type="radio" id="science" name="cat" value="science" />
+            <label htmlFor="science">Science</label>
+          </div>
+          <div className="cat">
+            <input type="radio" id="technology" name="cat" value="technology" />
+            <label htmlFor="technology">Technology</label>
+          </div>
+          <div className="cat">
+            <input type="radio" id="cinema" name="cat" value="cinema" />
+            <label htmlFor="cinema">Cinema</label>
+          </div>
+          <div className="cat">
+            <input type="radio" id="design" name="cat" value="design" />
+            <label htmlFor="design">Design</label>
+          </div>
+          <div className="cat">
+            <input type="radio" id="food" name="cat" value="food" />
+            <label htmlFor="food">Food</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Write;
